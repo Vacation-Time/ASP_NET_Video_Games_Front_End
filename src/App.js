60a7fa666c084264d88fc676cbc47a522e27d623
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-04-01 09:27:59 -0700

    Created New DisplayPlatform Files + Updated App.js File For Toubleshooting, Chart Not Yet Working

--- src/App.js
+++ src/App.js
@@ -2,26 +2,40 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DisplayGames from './Components/DisplayGames/DisplayGames.jsx';
 import NavBar from './Components/NavBar/NavBar.jsx';
+import './App.css';
 
 function App() {
 
-  const [entries, setEntries] = useState([]); // need to link to the database here?
+  const [allGames, setAllGames] = useState([]); 
+  const [displayGames, setDisplayGames] = useState([]);
+  const [game, setGame] = useState([]);
   const apiLink = "https://localhost:7260/api/games";
 
-  useEffect(() => { // gets list os songs when form is used
-    getVideoGameData();
-  },[])
+  useEffect(() => { //this is used to render the list of songs on App execution
+    getAllGames();
+    getDisplayGames();
+  }, [])
 
-  async function getVideoGameData(){
+  async function getAllGames(){
     let response = await axios.get(apiLink);
-    setEntries(response.data);
-    //console.log(response.data); // to view in console for testing
-  } 
+    setAllGames(response.data);
+  }
+  
+  async function getDisplayGames(){
+    let response = await axios.get(apiLink);
+    setDisplayGames(response.data);
+  }
+
+  // async function getGameById(game){
+  //   let response = await axios.get(`https://localhost:7260/api/games/${game.id}`)
+  //   setGame(response.data);
+  //   console.log(response.data)
+  // }
 
   return (
     <div className='page-container'>
       <div><NavBar/></div>
-      <div><DisplayGames displayGames = {entries} /></div> 
+      <div><DisplayGames parentGame = {game} /></div> 
     </div>
   );
 
